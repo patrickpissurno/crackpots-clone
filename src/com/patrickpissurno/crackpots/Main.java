7209b8a0183656504d4ab0f62ab83ef73610bbd0
Patrick Pissurno <contato@patrickpissurno.com.br>
2018-05-19 23:06:16 -0300

    Draw image

--- src/com/patrickpissurno/crackpots/Main.java
+++ src/com/patrickpissurno/crackpots/Main.java
@@ -8,16 +8,32 @@ public class Main {
         javax.swing.SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 
+    private static int posX = 0;
+    private static int posY = 0;
+
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("Crackpots");
         frame.setMinimumSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("//TODO: everything");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        frame.getContentPane().add(label);
+        JPanel panel = (JPanel) frame.getContentPane();
+        panel.setLayout(null);
+
+        JLabel label = new JLabel(new ImageIcon("sprite.png"));
+
+        label.setLocation(posX, posY);
+
+        Timer timer = new Timer( 100, a -> {
+            posX += 2;
+            posY += 2;
+            label.setLocation(posX, posY);
+        });
+        timer.start();
+
+        label.setBounds(100, 100, 32, 32);
+
+        panel.add(label);
 
         //Display the window.
         frame.pack();
