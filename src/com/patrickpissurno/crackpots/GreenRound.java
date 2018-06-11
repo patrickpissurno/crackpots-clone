f8ad7b52505518f3c4c268e1103065b29abc18f3
Patrick Pissurno <contato@patrickpissurno.com.br>
2018-06-11 19:14:44 -0300

    Implemented GreenRound and GreenSpider

--- src/com/patrickpissurno/crackpots/GreenRound.java
+++ src/com/patrickpissurno/crackpots/GreenRound.java
@@ -2,19 +2,19 @@ package com.patrickpissurno.crackpots;
 
 import javax.swing.*;
 
-public class RedRound extends Round{
+public class GreenRound extends Round{
     @Override
     protected ImageIcon getLifeIcon() {
-        return new ImageIcon("life_red.png");
+        return new ImageIcon("life_green.png");
     }
 
     @Override
     protected Round getNextRound() {
-        return new GreenRound();
+        return new BlackRound();
     }
 
     @Override
     protected Spider getNewSpider() {
-        return new RedSpider();
+        return new GreenSpider();
     }
 }
